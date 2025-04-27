d0d0d48addbe15d92a4e594a623d00e9500699b8
Полина <Vlasenkova-LoL@yandex.ru>
2025-04-27 22:28:02 +0300

    Revert "solution2"
    This reverts commit f681959e03d393b6a0cecd11ce24b691a9735a9b.

--- collect_files.sh
+++ collect_files.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-
 input_directory="$1"
 output_directory="$2"
-max_depth="$3"
 
 function recursion() {
   local input="$1"
   local result="$2"
-  local depth="$3"
-
-  if [[ "$depth" -eq 0 ]]
-    then
-    return
-  fi
 
   for element in "$input"/* 
   do  
     if [[ -f "$element" ]] 
     then
-      name=$(basename "$element")
+      name=$(basename "$file")
       cp "$element" "$result/$name"
     elif [[ -d "$element" ]]
     then
-      changed=$((depth - 1))
-      recursion "$element" "$result" "$changed"
+      recursion "$element" "$result"
     fi
   done
 }
 
-recursion "$input_directory" "$output_directory" "$max_depth"
+recursion "$input_directory" "$output_directory"
 
 
 
