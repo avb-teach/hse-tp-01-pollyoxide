66b7321554b19c394f178d207a0142bd4bdfb827
Полина <Vlasenkova-LoL@yandex.ru>
2025-04-27 18:54:00 +0300

    solution1

--- collect_files.sh
+++ collect_files.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-input="$1"
-output="$2"
-max_depth=""
+input_directory="$1"
+output_directory="$2"
+
+function recursion() {
+  local input="$1"
+  local result="$2"
+
+  for element in "$input"/* 
+  do
+    if [[ -f "$element" ]] 
+    then
+      name=$(basename "$file")
+      cp "$element" "$result/$name"
+    elif [[ -d "$element" ]]
+    then
+      recursion "$element" "$result"
+    fi
+  done
+}
+
+recursion "$input_directory" "$output_directory"
+
+
+
+
